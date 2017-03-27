add4b045bf27a6486bee21db62e0b37303d9f604
Marcio Paiva <marcioapf@gmail.com>
2017-03-27 09:05:16 -0300

    Classes Point and Cell

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,42 +1,101 @@
 class Minesweeper
+	attr_accessor :width
+	attr_accessor :height
+	attr_accessor :board
+
 	def initialize(width, height, n_mines)
 		@width = width
 		@height = height
 		@n_mines = n_mines
+		@board = Array.new(height) { |line| Array.new(width) { |col| Cell.new(Point.new(line, col)) } }
 	end
 
-	def print_params()
-		puts "width = " + @width.to_s
-		puts "height = " + @height.to_s
-		puts "number of bombs = " + @n_mines.to_s
+	def print_board(xray = false)
+		@board.each { |line|
+			line.each { |cell|
+				 if cell.flag?
+					print "F"
+				 elsif xray and !cell.open? and cell.bomb?
+					print "B"
+				 elsif !cell.open?
+					print "C"
+				 else
+					print n_surrounding_bombs(cell.point).to_s
+				 end
+			}
+			print "\n"
+		}
+		print "\n\n"
+	end
+
+	# private
+	def is_valid(p)
+		p.x >= 0 and p.x < @height and p.y >= 0 and p.y < @width
+	end
+
+	# private
+	def neighbors(p)
+		[
+			Point.new(p.x-1, p.y), Point.new(p.x-1, p.y+1),
+			Point.new(p.x, p.y+1), Point.new(p.x+1, p.y+1),
+			Point.new(p.x+1, p.y), Point.new(p.x+1, p.y-1),
+			Point.new(p.x, p.y-1), Point.new(p.x-1, p.y-1)
+		].select { |p| is_valid(p) }
+	end
+
+	def n_surrounding_bombs(p)
+		neighbors(p).count{|n| cell_at(n).bomb?}
+	end
+
+	def cell_at(p)
+		@board[p.x][p.y]
 	end
 end
 
-engine = Minesweeper.new(10, 10, 9) # 10x10 and 9 bombs
-engine.print_params
+class Cell
+	attr_accessor :flag
+	alias_method :flag?, :flag
 
-=begin
+	attr_accessor :bomb
+	alias_method :bomb?, :bomb
 
-Intended usage example
+	attr_accessor :open
+	alias_method :open?, :open
 
-width, height, num_mines = 10, 20, 50
-game = Minesweeper.new(width, height, num_mines)
+	attr_accessor :point
 
-while game.still_playing?
-  valid_move = game.play(rand(width), rand(height))
-  valid_flag = game.flag(rand(width), rand(height))
-  if valid_move or valid_flag
-  printer = (rand > 0.5) ? SimplePrinter.new : PrettyPrinter.new
-  printer.print(game.board_state)
-  end
+	def initialize(point)
+		self.bomb = false
+		self.flag = false
+		self.open = false
+		self.point = point
+	end
 end
 
-puts "Fim do jogo!"
-if game.victory?
-  puts "Você venceu!"
-else
-  puts "Você perdeu! As minas eram:"
-  PrettyPrinter.new.print(game.board_state(xray: true))
-end
+class Point
+	attr_accessor :x
+	attr_accessor :y
+
+	def initialize(x, y)
+		@x = x
+		@y = y
+	end
+
+	def ==(o)
+		o.class == self.class && o.state == state
+	end
 	
-=end
+	def state
+		[@x, @y]
+	end
+
+	def to_s
+		"(#{self.x}, #{self.y})"
+	end
+
+	alias_method :eql?, :==
+
+	def hash
+		state.hash
+	end
+end
