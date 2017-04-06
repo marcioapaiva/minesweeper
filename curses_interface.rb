44cb25dde0da3d918bed44528564fdb077454f8e
Marcio Paiva <marcioapf@gmail.com>
2017-04-06 01:12:27 -0300

    First(crappy) attempt to generalize window drawing

--- curses_interface.rb
+++ curses_interface.rb
@@ -1,7 +1,87 @@
 require "curses"
 require "./minesweeper_engine.rb"
 
+# MWindow => Managed window. A layer of abstraction over a
+# curses window
+class MWindow
+	attr_accessor :width
+	attr_accessor :height
+	attr_accessor :cwin # courses window
+
+	def initialize(height, width)
+		self.cwin = Curses::Window.new(width, height, 0, 0)
+		self.height = height
+		self.width = width
+	end
+
+	# type is one of :center, :top, :bottom
+	def anchor_y(type)
+		@anchor_y_type = type
+		@anchor_y_pos = Proc.new
+	end
+
+	# type is one of :center, :left, :right
+	def anchor_x(type)
+		@anchor_x_type = type
+		@anchor_x_pos = Proc.new
+	end
+
+	def reposition
+		cwin.resize(height, width)
+		cwin.move(calc_pos(@anchor_y_type, @anchor_y_pos.call, height),
+				  calc_pos(@anchor_x_type, @anchor_x_pos.call, width))
+	end
+
+	def redraw(bs)
+	end
+
+	def draw(bs)
+	end
+
+	def calc_pos(type, pos, size)
+		case type
+		when :top, :left
+			pos
+		when :center
+			pos - size/2
+		when :bottom, :right
+			pos - size
+		end
+	end
+end
+
+class BoardWindow < MWindow
+	def initialize(height, width, interface)
+		@interface = interface
+		super(height, width)
+	end
+
+	def redraw(bs)
+		self.cwin.box("|", "=")
+		self.draw(bs)
+	end
+
+	def draw(bs)
+		if bs == nil
+			return
+		end
+		bs.each_with_index do |array_symbols, y|
+			array_symbols.each_with_index do |cell_hash, x|
+				cwin.setpos(y + 1, x*3 + 1)
+				chr = CursesInterface.get_cell_char(cell_hash)
+				if x == @interface.pos.x and y == @interface.pos.y
+					cwin.addstr "[#{chr}]"
+				else
+					cwin.addstr " #{chr} "
+				end
+			end
+		end
+	end
+end
+
 class CursesInterface
+	attr_accessor :pos
+
 	def self.init
 		Curses.init_screen
 		Curses.nl
@@ -11,37 +91,39 @@ class CursesInterface
 	end
 
 	def initialize(width, height)
-		@pos = MinesweeperEngine::Point.new(0, 0)
+		self.pos = MinesweeperEngine::Point.new(0, 0)
 		@width = width
 		@height = height
-		win_height = @height + 2
-		win_width = 3*@width + 2
-		@bwin = Curses::Window.new(0, 0, 0, 0)
-		redraw
+
+		@mwindows = []
+		@bwin = BoardWindow.new(@height + 2, 3*@width + 2, self)
+		@bwin.anchor_x(:center) {Curses.cols/2}
+		@bwin.anchor_y(:center) {Curses.lines/2}
+		@mwindows << @bwin
+
+		redraw(nil)
 	end
 
-	def update_bwin_location
-		win_height = @height + 2
-		win_width = 3*@width + 2
-		y_pos = (Curses.lines - win_height)/2
-		x_pos = (Curses.cols - win_width)/2
-		if y_pos > 0 and x_pos > 0
-			@bwin.resize(win_height, win_width)
-			@bwin.move(y_pos, x_pos)
-		end
-	end
-
-	def redraw(board_state = nil)
+	def redraw(bs)
 		Curses.clear
 		print_welcome
 		Curses.refresh
 
-		update_bwin_location
+		@bwin.reposition
 
-		@bwin.clear
-		@bwin.box("|", "=")
-		print(board_state) if board_state
-		@bwin.refresh
+		for mwindow in @mwindows
+			mwindow.cwin.clear
+			mwindow.redraw(bs)
+			mwindow.draw(bs)
+			mwindow.cwin.refresh
+		end
+	end
+
+	def draw(bs)
+		for mwindow in @mwindows
+			mwindow.draw(bs)
+			mwindow.cwin.refresh
+		end
 	end
 
 	def print_welcome
@@ -59,37 +141,28 @@ class CursesInterface
 	end
 
 	def print(board_state)
-		board_state.each_with_index do |array_symbols, y|
-			array_symbols.each_with_index do |cell_hash, x|
-				@bwin.setpos(y + 1, x*3 + 1)
-				chr = get_cell_char(cell_hash)
-				if x == @pos.x and y == @pos.y
-					@bwin.addstr "[#{chr}]"
-				else
-					@bwin.addstr " #{chr} "
-				end
-			end
-		end
+		
 
 		# Curses.refresh
-		@bwin.refresh
+		@bwin.cwin.refresh
 	end
 
 	def get_move(board_state)
 		move_type = nil
 
 		while true
-			self.print(board_state)
+			# self.print(board_state)
+			self.draw(board_state)
 			c = Curses.getch
 			case c
 			when Curses::KEY_UP
-				@pos.y -= 1 if @pos.y > 0
+				pos.y -= 1 if pos.y > 0
 			when Curses::KEY_DOWN
-				@pos.y += 1 if @pos.y < @height - 1
+				pos.y += 1 if pos.y < @height - 1
 			when Curses::KEY_LEFT
-				@pos.x -= 1 if @pos.x > 0
+				pos.x -= 1 if pos.x > 0
 			when Curses::KEY_RIGHT
-				@pos.x += 1 if @pos.x < @width - 1
+				pos.x += 1 if pos.x < @width - 1
 			when "f", "F"
 				move_type = :flag
 				break
@@ -99,14 +172,14 @@ class CursesInterface
 			when Curses::KEY_RESIZE
 				redraw(board_state)
 			when "q",  "Q"
-				@bwin.close
+				@bwin.cwin.close
 				exit 0
 			else
 				print_bottom("Unknown command #{Curses.keyname(c)}") if c != nil
 			end
 		end
 
-		return {:type => move_type, :pos => @pos}
+		return {:type => move_type, :pos => pos}
 	end
 
 	def inform_win
@@ -131,7 +204,7 @@ class CursesInterface
 		open_bomb: 'X'
 	}
 
-	def get_cell_char(cell_hash)
+	def self.get_cell_char(cell_hash)
 		if cell_hash[:type] == :clear and cell_hash[:n_surr] != 0
 			cell_hash[:n_surr].to_s
 		else
